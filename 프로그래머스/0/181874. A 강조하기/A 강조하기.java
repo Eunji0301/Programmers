cca48772d2dac5f3b3acccf7976f91d846afca82
Eunji0301 <forstudy.eunji@gmail.com>
2024-11-04 14:20:38 +0900

    [level 0] Title: A 강조하기, Time: 0.14 ms, Memory: 75.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181874. A 강조하기/A 강조하기.java
@@ -0,0 +1,9 @@
+class Solution {
+    public String solution(String myString) {
+        String answer = myString;
+        answer = answer.toLowerCase();
+        answer = answer.replaceAll("a","A");
+
+        return answer;
+    }
+}
